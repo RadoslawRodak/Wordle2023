792c271949c9bd0c4e17a297a89fc4ff99071741
RadoslawRodak <129396102+RadoslawRodak@users.noreply.github.com>
2024-01-02 18:04:36 +0000

    first commit

---
+++ Wordle2023/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Wordle2023
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Wordle2023/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Wordle2023
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
